9ce858f6068388ac9a5a9d214a37afa65f4b8234
Fredy Rincon <frincon@healthconnex.com.au>
2016-09-06 21:28:44 +1000

    no message

--- app.js
+++ app.js
@@ -16,11 +16,25 @@ Person.prototype.sayHello = function () {
     return 'My name is: ' + this.name;
 };
 
+var Reminder = function (pId) {
+    this.reminderId = 'This is the id of reminder' + pId;
+};
+
+
 app.get('/', function (req, res) {
   var fredy = new Person('Fredy Rincon', 'Mobile Developer', 'HCX');
   //res.send('Hello ' + fredy.sayHello());
   //res.send(JSON.stringify({ a: 1 }));
   res.send(JSON.stringify(fredy));
+});
+
+app.get('/reminder/:id', function (req, res, next) {
+  /*Todo.findById(req.params.id, function(err, todo){
+    if(err) res.send(err);
+    res.json(todo);
+  });*/
+  var each = new Reminder(req.params.id);
+  res.json(each);
 });
 
 
